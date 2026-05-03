debd6fb46f4a018028a41384a0b7c0d9a6472ae9
miguel almeida <mguel3104@gmail.com>
2026-05-03 16:55:16 -0300

    ghjf

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,18 @@
+let passo = 1
+while (passo <= 20) {
+    console.log(`Passo ${passo}`)
+    passo++
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
