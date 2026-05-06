410d28d98f57962f924d951b86f238bef6a82821
miguel almeida <mguel3104@gmail.com>
2026-05-06 16:30:04 -0300

    d

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -1,14 +1,18 @@
 let passo = 1
+do  {
+    console.log(`Passo ${passo}`)
+    passo++
+} while (passo <= 20)
+
+/*
+
+let passo = 1
 while (passo <= 20) {
     console.log(`Passo ${passo}`)
     passo++
 }
 
 
-
-
-
-/*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
